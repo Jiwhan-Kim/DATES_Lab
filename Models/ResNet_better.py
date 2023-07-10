a53ce46ded32d4ff920bbebfad1d25b8da11b2e6
juneeee <june029882@yonsei.ac.kr>
2023-07-10 21:51:47 +0900

    better resnet uploaded

---
+++ Models/ResNet_better.py
@@ -0,0 +1,80 @@
+import torch.nn as nn
+class BasicBlock(nn.Module):
+
+    def __init__(self, in_channels, out_channels, stride):
+        super(BasicBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1, bias=False) # stride = 1 or 2
+        
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        
+
+        self.shortcut = nn.Sequential()
+        if stride != 1:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride, padding=0, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+
+    def forward(self, x):
+        residual = x
+        
+        out = self.bn1(x)
+        out = nn.ReLU()(out)
+        out = self.conv1(out)
+
+        out = self.bn2(out)
+        out = nn.ReLU()(out)
+        out = self.conv2(out)
+
+        out = out + self.shortcut(residual)
+
+        return out
+
+class ResNet_better(nn.Module):
+    def __init__(self):
+        super(ResNet_better, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+
+        self.layer1 = self.make_layer(64, 64, 1) # first block stride 1
+        self.layer2 = self.make_layer(64, 128, 2)
+        self.layer3 = self.make_layer(128, 256, 2)
+        self.layer4 = self.make_layer(256, 512, 2)
+
+        self.fc = nn.Linear(1*1*512, 10)
+        # Initialize the weights using Kaiming initialization for the fully connected layer
+        nn.init.kaiming_uniform_(self.fc.weight, mode='fan_in', nonlinearity='relu')
+
+
+    def make_layer(self, in_channels, out_channels, stride):
+        layers=[]
+        if stride == 1:
+            layers.append(BasicBlock(in_channels, out_channels, 1))
+            layers.append(BasicBlock(out_channels, out_channels, 1))
+        elif stride == 2:
+            layers.append(BasicBlock(in_channels, out_channels, 2))
+            layers.append(BasicBlock(out_channels, out_channels, 1))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        
+        x = self.bn1(x)
+        x = nn.ReLU()(x)
+        x = nn.MaxPool2d(2, 2)(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = nn.MaxPool2d(2, 2)(x)
+
+        x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+
+        return x
+
---
+++ train_ResNet_better.py
@@ -0,0 +1,131 @@
+'''
+DATES Lab
+Model Check by CIFAR10
+Training
+Made by KimJW (SSE21)
+'''
+
+import torch
+import torch.nn
+import numpy as np
+from tqdm import tqdm
+from os import path
+
+import Models   as M
+import Trainers as T
+import Datas    as D
+
+# Set Devices (M1/M2 mps, NVIDIA cuda:0, else cpu)
+device = None
+if   torch.backends.mps.is_available():
+    device = "mps"
+elif torch.cuda.is_available():
+    device = "cuda:0"
+else:
+    device = "cpu"
+
+
+
+
+# Global Data
+train_size = 40_000
+batch_size = 64
+epoch      = 100
+
+
+def train(loader, n_epoch):
+    loss = 0
+    model.train()
+    pbar = tqdm(loader)
+    for image, label in pbar:
+        x = image.to(device)
+        y = label.to(device)
+        loss = trainer.step(x, y)
+        pbar.set_description("Training Epoch %3d, %2.6f" % (n_epoch, loss))
+
+
+def evaluate(loader, n_epoch):
+    with torch.no_grad():
+        correct = 0
+        model.eval()
+        evaluatelosssum = 0
+        result_pbar = tqdm(loader)
+        for image, label in result_pbar:
+            x = image.to(device)
+            y = label.to(device)
+            output = model.forward(x)
+            evaluatelosssum = evaluatelosssum + torch.nn.CrossEntropyLoss()(output, y)
+            result = torch.argmax(output, dim=1)
+            for res, ans in zip(result, y):
+                if res == ans:
+                    correct += 1
+        print("Epoch {}. Accuracy: {}".format(n_epoch, 100 * correct / (50000 - train_size)))
+    return evaluatelosssum
+
+
+if __name__ == "__main__":
+    print("running train.py")
+    print("Device on Working: ", device)
+    torch.cuda.empty_cache()
+    model   = M.ResNet_better().to(device)
+    trainer = T.SGDMC_Trainer_ResNet(0.01, model, device)
+ 
+    patience = 3  # loss가 일정 에포크 동안 감소하지 않으면 lr decrease
+    
+
+    no_improvement_count = 0  # 개선이 없는 에포크 카운트
+
+    train_load, valid_load, test_load = D.Load_CIFAR10(train_size, batch_size)
+
+    if path.exists("./model_params_ResNet_better.pth"):
+        model.load_state_dict(torch.load("./model_params_ResNet_better.pth"))
+
+
+    for i in range(epoch):
+        train(train_load, i)
+        loss_return = evaluate(valid_load, i)
+        if i==0:
+          no_improvement_count = 0
+          best_eval_loss = loss_return
+          torch.save(model.state_dict(), 'model_params_ResNet_better.pth')
+          
+        else:
+          if loss_return >= best_eval_loss:
+            no_improvement_count += 1
+          else:
+            no_improvement_count = 0
+            best_eval_loss = loss_return
+            torch.save(model.state_dict(), 'model_params_ResNet_better.pth')
+        if no_improvement_count >= patience and trainer.lr > 0.001:
+            no_improvement_count = 0
+            trainer.lr = trainer.lr / 10
+            model.load_state_dict(torch.load("./model_params_ResNet_better.pth")) # take best one
+            print("LR decreased")
+        elif no_improvement_count >= patience and trainer.lr == 0.001:
+            no_improvement_count = 0
+            print("no improvement")
+            break
+
+    with torch.no_grad():
+        model.load_state_dict(torch.load("./model_params_ResNet_better.pth"))
+        model.eval()
+        val = np.zeros(10, dtype=int)
+        correct = 0
+
+        result_pbar = tqdm(test_load)
+        for image, label in result_pbar:
+            x = image.to(device)
+            y = label.to(device)
+            output = model.forward(x)
+            result = torch.argmax(output, dim=1)
+
+            for res, ans in zip(result, y):
+                if res == ans:
+                    val[res] += 1
+                    correct += 1
+        
+        print("Final Accuracy: {}\n\n".format(100 * correct / 10000))
+        for i in range(10):
+            print("class {}: {} / 1000".format(i, val[i]))
+
+    
